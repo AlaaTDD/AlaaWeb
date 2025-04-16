6995d646c6a82dd3e04a3ed411df555b3c8d169b
AlaaTDD <alaat7058@gmail.com>
2025-04-16 09:39:41 -0700

    ayattalaaweb

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -11,9 +11,9 @@ const Contact = () => {
               <span className="w-[30px] h-[2px] bg-primary"></span>
               Say Hello 👋
             </div>
-            <h1 className="h1 max-w-md mb-8">Let`s Work Together.</h1>
+            <h1 className="h1 max-w-md mb-8">Let`s Contact Together.</h1>
             <p className="subtitle max-w-[400px]">
-            Please contact us so that I can help you and build your project. Contact us and inform us of the details.
+            Please contact us so that I can help you. Contact us and inform us of the details.
             </p>
           </div>
           <div className="hidden xl:flex w-full bg-contact dark:bg-contact bg-contain bg-top bg-no-repeat"></div>
@@ -22,19 +22,19 @@ const Contact = () => {
           <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
             <div className="flex items-center gap-x-8">
               <MailIcon size={18} className="text-primary" />
-              <div>alaatahadev048@gmail.com</div>
+              <div>alaattaha608@gmail.com</div>
             </div>
             <div className="flex items-center gap-x-8">
               <HomeIcon size={18} className="text-primary" />
-              <div>Egypt,Cairo</div>
+              <div>Egypt,Qalyubia</div>
             </div>
             <div className="flex items-center gap-x-8">
               <PhoneIcon size={18} className="text-primary" />
-              <div>+201013961667</div>
+              <div>+201507948882</div>
             </div>
           </div>
-          <Form/>
-        </div>
+{/* <Form /> */}
+</div>
       </div>
     </section>
   );
